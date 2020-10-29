98896b78dd03f3932f8f2ef1a3321c8d3b0736c0
Ionhedblom <ionysoshedblom@gmail.com>
2020-10-29 17:40:38 +0100

    Details funkar

--- js/app.js
+++ js/app.js
@@ -1,51 +1,80 @@
-const btnRight = document.querySelector('.btn-right');
-const btnLeft = document.querySelector('.btn-left');
-const characterList = document.querySelector('.character-list');
-const pageCounter = document.querySelector('.page-counter');
-let characterListALL = document.querySelectorAll('.character-list li')
-let page = 1;
+async function loadCatalog() {
+  const btnRight = document.querySelector('.btn-right');
+  const btnLeft = document.querySelector('.btn-left');
+  const characterList = document.querySelector('.character-list');
 
-fetchPage();
-async function fetchPage() {
-    let request = await fetch(`http://swapi.dev/api/people/?page=${page}`);
-    let response = await request.json();
-    const characters = response.results;
-    createListItems(characters);
-   
-  }
-  console.log(characterListALL)
-function createListItems(list) {
-  list.forEach(character => {
-    const listItem = document.createElement('li');
-    listItem.classList.add('character-item');
-    listItem.innerText = character.name;
-    characterList.append(listItem);
-    
-  });
-}
+  const name = document.querySelector('.name');
+  const height = document.querySelector('.height');
+  const mass = document.querySelector('.mass');
+  const hairColor = document.querySelector('.hair-color');
+  const skinColor = document.querySelector('.skin-color');
+  const eyeColor = document.querySelector('.eye-color');
+  const birthYear = document.querySelector('.birth-year');
+  const gender = document.querySelector('.gender');
 
-btnRight.addEventListener('click', () => {
-  while (characterList.firstChild) {
-    characterList.removeChild(characterList.firstChild);
-  }
-  if(page !== 8){
-      page++
-      pageCounter.innerText = page;
-  }
+  const pageCounter = document.querySelector('.page-counter');
+
+  let page = 1;
+
   fetchPage();
-});
 
-btnLeft.addEventListener('click', () => {
+  async function fetchPage() {
+    const request = await fetch(`http://swapi.dev/api/people/?page=${page}`);
+    const response = await request.json();
+    const characters = response.results;
+
+    characters.forEach(character => {
+      const listItem = document.createElement('li');
+      listItem.classList.add('character-item');
+      listItem.innerText = character.name;
+      characterList.append(listItem);
+      listItem.addEventListener('click', () => {
+        name.innerText = listItem.innerText;
+        height.innerText = `Height: ${character.height}`;
+        mass.innerText = `Mass: ${character.mass}kg`;
+        hairColor.innerText = `Hair color: ${character.hair_color}`;
+        skinColor.innerText = `Skin color: ${character.skin_color}`;
+        eyeColor.innerText = `Eye color: ${character.eye_color}`;
+        birthYear.innerText = `Birth year: ${character.birth_year}`;
+        gender.innerText = `Gender: ${character.gender}`;
+      });
+    });
+  }
+
+  // function createListItems(list) {
+  //   list.forEach(character => {
+  //     const listItem = document.createElement('li');
+  //     listItem.classList.add('character-item');
+  //     listItem.innerText = character.name;
+  //     characterList.append(listItem);
+  //     listItem.addEventListener('click', () => {
+  //       console.log(listItem.innerText);
+  //     });
+  //   });
+  // }
+
+  btnRight.addEventListener('click', () => {
+    while (characterList.firstChild) {
+      characterList.removeChild(characterList.firstChild);
+    }
+    if (page !== 8) {
+      page++;
+      pageCounter.innerText = page;
+    }
+    fetchPage();
+  });
+
+  btnLeft.addEventListener('click', () => {
     while (characterList.firstChild) {
       characterList.removeChild(characterList.firstChild);
     }
 
-    if(page !== 1){
-        page--;
-        pageCounter.innerText = page;
+    if (page !== 1) {
+      page--;
+      pageCounter.innerText = page;
     }
     fetchPage();
-   
   });
+}
 
-
+loadCatalog();
