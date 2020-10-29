da60c549fa132f76aaa01a754de6e0bd0ee33d97
Ionhedblom <ionysoshedblom@gmail.com>
2020-10-29 16:04:24 +0100

    Bläddra funkar

--- js/app.js
+++ js/app.js
@@ -1,40 +1,31 @@
-function starwars() {
-  const characterList = document.querySelector('.character-list');
-  
+const btn = document.querySelector('.btn-right');
+const characterList = document.querySelector('.character-list');
+const pageCounter = document.querySelector('.page-counter');
+let page = 1;
 
+fetchPage();
 
-  async function loadCharacterList() {
-    
-    let people = await fetch('https://swapi.dev/api/people/');
-    
-    const data = await people.json();
-    
-    let results = data.results;
-    
-
-    
-    
-
-    
-
-   
-
-    
-    results.forEach(result => {
-      const character = document.createElement('li');
-      characterList.append(character);
-      character.innerText = result.name;
-      character.classList.add('character-item');
-
-      character.addEventListener('click', () => {
-        
-      })
-    });
-  }
-
-  
-  loadCharacterList();
-  
+function createListItems(list) {
+  list.forEach(character => {
+    const listItem = document.createElement('li');
+    listItem.classList.add('character-item');
+    listItem.innerText = character.name;
+    characterList.append(listItem);
+  });
 }
 
-starwars();
+btn.addEventListener('click', () => {
+  while (characterList.firstChild) {
+    characterList.removeChild(characterList.firstChild);
+  }
+  page++;
+  pageCounter.innerText = page;
+  fetchPage();
+});
+
+async function fetchPage() {
+  let request = await fetch(`http://swapi.dev/api/people/?page=${page}`);
+  let response = await request.json();
+  const characters = response.results;
+  createListItems(characters);
+}
